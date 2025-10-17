0703a930b1a14260cc0d1867a3539142e5cf9cb4
Nikulin V. <viacheslav.nikulin@freedompay.kz>
2025-10-17 20:00:00 +0500

    fix check

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,4 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+import { expect, test } from '@playwright/test';
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
