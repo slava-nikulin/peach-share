998508dba4eba4ffe7cde542c38255511ebad024
Nikulin V. <viacheslav.nikulin@freedompay.kz>
2026-03-01 21:51:39 +0500

    fix: cd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,4 +4,8 @@ import type { Component } from 'solid-js';
 import { routes } from '../router/routes';
 import { RootLayout } from './RootLayout';
 
-export const App: Component = () => <Router root={RootLayout}>{routes}</Router>;
+export const App: Component = () => (
+  <Router base={import.meta.env.BASE_URL} root={RootLayout}>
+    {routes}
+  </Router>
+);
